4d6cf1abd6541f42700f96b13025cc5587891f0d
Hiel Jorn <jorn.hiel@proximus.be>
2018-06-17 21:28:57 +0200

    v1

--- src/main/java/jorn/hiel/view/MainScreen.java
+++ src/main/java/jorn/hiel/view/MainScreen.java
@@ -60,14 +60,18 @@ public class MainScreen implements Initializable {
 
 
             //details
-            // day1Detail,day2Detail,day3Detail,day4Detail,day5Detail,day6Detail,day7Detail,day8Detail,day9Detail,day10Detail,day11Detail,day12Detail,day13Detail,day14Detail,day15Detail,day16Detail,
+            day1Detail,day2Detail,day3Detail,day4Detail,day5Detail,day6Detail,day7Detail,day8Detail,day9Detail,day10Detail,day11Detail,day12Detail,day13Detail,day14Detail,day15Detail,day16Detail,
             day17Detail,day18Detail,day19Detail,day20Detail,day21Detail,day22Detail,day23Detail,day24Detail,day25Detail,day26Detail,day27Detail,
             day28Detail,day29Detail,day30Detail,day31Detail;
 
 
 
     @FXML
-    List<Label> labels = new ArrayList<Label>();
+    private List<Label> labels = new ArrayList<Label>();
+    private List<TextField> urenTextFields = new ArrayList<TextField>();
+    private List<TextField> extraTextFields = new ArrayList<TextField>();
+    private List<TextField> detailsTextFields = new ArrayList<TextField>();
+
 
 
 
@@ -89,12 +93,14 @@ public class MainScreen implements Initializable {
     @FXML
     public void nextMonth(){
         firstDayOfMonth=firstDayOfMonth.plusMonths(1);
+        clearTextfields();
         setDateLabels();
     }
 
     @FXML
     public void previousMonth(){
         firstDayOfMonth=firstDayOfMonth.minusMonths(1);
+        clearTextfields();
         setDateLabels();
     }
 
@@ -102,8 +108,28 @@ public class MainScreen implements Initializable {
     public void dateToCurrentMonth()
     {
         firstDayOfMonth=LocalDate.of(werkUren.getCurrentYear(),werkUren.getCurrentMonth(),1);
+        clearTextfields();
         setDateLabels();
     }
+
+    @FXML
+    public void clearTextfields()
+    {
+        for (TextField textField: detailsTextFields
+                ) {textField.setText("");
+
+        }
+        for (TextField textField: urenTextFields
+                ) {textField.setText("");
+
+        }
+        for (TextField textField: extraTextFields
+                ) {textField.setText("");
+
+        }
+    }
+
+
 
     @FXML
     public void verifyVisibility()
@@ -139,26 +165,42 @@ public class MainScreen implements Initializable {
         labels.addAll(Arrays.asList(day1,day2,day3,day4,day5,day6,day7,day8,day9,day10,day11,day12,day13,day14,day15,day16,
                 day17,day18,day19,day20,day21,day22,day23,day24,day25,day26,day27,
                 day28,day29,day30,day31));
-
-
-
+        urenTextFields.addAll(Arrays.asList(day1Uren,day2Uren,day3Uren,day4Uren,day5Uren,day6Uren,day7Uren,day8Uren,day9Uren,day10Uren,day11Uren,day12Uren,day13Uren,day14Uren,day15Uren,day16Uren,
+                day17Uren,day18Uren,day19Uren,day20Uren,day21Uren,day22Uren,day23Uren,day24Uren,day25Uren,day26Uren,day27Uren,
+                day28Uren,day29Uren,day30Uren,day31Uren));
+        detailsTextFields.addAll(Arrays.asList(day1Detail,day2Detail,day3Detail,day4Detail,day5Detail,day6Detail,day7Detail,day8Detail,day9Detail,day10Detail,day11Detail,day12Detail,day13Detail,day14Detail,day15Detail,day16Detail,
+                day17Detail,day18Detail,day19Detail,day20Detail,day21Detail,day22Detail,day23Detail,day24Detail,day25Detail,day26Detail,day27Detail,
+                day28Detail,day29Detail,day30Detail,day31Detail));
+        extraTextFields.addAll(Arrays.asList(day1Extra,day2Extra,day3Extra,day4Extra,day5Extra,day6Extra,day7Extra,day8Extra,day9Extra,day10Extra,day11Extra,day12Extra,day13Extra,day14Extra,day15Extra,day16Extra,
+                day17Extra,day18Extra,day19Extra,day20Extra,day21Extra,day22Extra,day23Extra,day24Extra,day25Extra,day26Extra,day27Extra,
+                day28Extra,day29Extra,day30Extra,day31Extra));
 
     }
 
     public void setDateLabels(){
-        LocalDate teller;
+        LocalDate teller,reset;
         monthLabel.setText(String.valueOf(firstDayOfMonth.getMonth()));
         yearLabel.setText(String.valueOf(firstDayOfMonth.getYear()));
         verifyVisibility();
 
 
         //reset to the 1st of the month
-        teller=LocalDate.of(firstDayOfMonth.getYear(),firstDayOfMonth.getMonth(),1);
+        reset=LocalDate.of(firstDayOfMonth.getYear(),firstDayOfMonth.getMonth(),1);
+        teller=reset;
 
         for (Label label:labels) {
             String splitter="\t\t";
             if(teller.getDayOfWeek().toString().equals("WEDNESDAY")){ splitter="\t";}
             label.setText(String.valueOf(teller.getDayOfWeek()).toLowerCase() + splitter+ "-\t"  + teller.getDayOfMonth()  );
+            teller=teller.plusDays(1);
+        }
+
+        teller=reset;
+
+        for (TextField textfield: detailsTextFields)
+        {
+            if(teller.getDayOfWeek().toString().equals("SATURDAY")||teller.getDayOfWeek().toString().equals("SUNDAY")){textfield.setText("weekend");
+            }
             teller=teller.plusDays(1);
         }
 
@@ -182,8 +224,6 @@ public class MainScreen implements Initializable {
 
 
 
-        //monthTable.setItems(FXCollections.observableArrayList(month));
-        //System.out.println(monthTable);
 
 
     }
